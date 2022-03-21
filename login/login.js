6e659e436d69a0dd593c0d129e8d94b63a235a4c
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-03-22 01:54:48 +0200

    Added fetch for registration.

--- login/login.js
+++ login/login.js
@@ -6,38 +6,38 @@ submitButton.onclick = (event) => {
     let loginForm = document.getElementById("login-form");
     if (loginForm.checkValidity()) {
         event.preventDefault();
+        loginForm = loginForm.getElementsByTagName('input');
+
+        let fields = ['username', 'password'];
+
+        let body = {};
+        fields.forEach((key) => {
+                body[key] = loginForm[key].value;
+            }
+        )
+        fetch('http://127.0.0.1:8000/auth/login/', {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json; charset=UTF-8'}
+        }).then((response) => {
+            if (response.status === 200) {
+                response.json().then((data) => {
+                    setCookie("access", data['access'], 60);
+                    setCookie('refresh', data['refresh'], 60 * 24 * 15);
+                    window.location.href = '../problems/problems.html';
+                })
+            } else {
+                response.json().then((data) => {
+                    throw data.detail;
+                }).catch(e => {
+                    if (e) {
+                        error.innerHTML = e;
+                        error.style = "font-size: 12px; color: red; font-family: \"Dubai Light\", monospace;"
+                    }
+                });
+            }
+        }).catch((e) => {
+            error.innerHTML = e.message;
+        })
     }
-    loginForm = loginForm.getElementsByTagName('input');
-
-    let fields = ['username', 'password'];
-
-    let body = {};
-    fields.forEach((key) => {
-            body[key] = loginForm[key].value;
-        }
-    )
-    fetch('http://127.0.0.1:8000/auth/login/', {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {'Content-Type': 'application/json; charset=UTF-8'}
-    }).then((response) => {
-        if (response.status === 200) {
-            response.json().then((data) => {
-                setCookie("access", data['access'], 60);
-                setCookie('refresh', data['refresh'], 60 * 24 * 15);
-                window.location.href = '../problems/problems.html';
-            })
-        } else {
-            response.json().then((data) => {
-                throw data.detail;
-            }).catch(e => {
-                if (e) {
-                    error.innerHTML = e;
-                    error.style = "font-size: 12px; color: red; font-family: \"Dubai Light\", monospace;"
-                }
-            });
-        }
-    }).catch((e) => {
-        error.innerHTML = e.message;
-    })
 }
--- registration/register.js
+++ registration/register.js
@@ -1,3 +1,43 @@
-document.querySelector('.submit-button').addEventListener(onclick, () => {
-    alert("message");
-})
+let submitButton = document.querySelector('.submit-button');
+let error = document.querySelector('.error');
+submitButton.onclick = (event) => {
+    let loginForm = document.getElementById("register-form");
+    if (loginForm.checkValidity()) {
+        event.preventDefault();
+        loginForm = loginForm.getElementsByTagName('input');
+
+        let fields = ['email', 'username', 'first_name', 'last_name', 'password', 'confirm_password'];
+
+        let body = {};
+        fields.forEach((key) => {
+                body[key] = loginForm[key].value;
+            }
+        )
+        if (body['password'] !== body['confirm_password']) {
+            error.innerHTML = "Passwords didn't match.";
+            error.style = "font-size: 12px; color: red; font-family: \"Dubai Light\", monospace;"
+            return
+        }
+        fetch('http://127.0.0.1:8000/auth/register/', {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json; charset=UTF-8'}
+        }).then((response) => {
+            if (response.status === 201) {
+                window.location.href = '../login/login.html';
+            } else {
+                response.json().then((data) => {
+                    throw data.detail;
+                }).catch(e => {
+                    if (e) {
+                        error.innerHTML = e;
+                        error.style = "font-size: 12px; color: red; font-family: \"Dubai Light\", monospace;"
+                        console.log(e);
+                    }
+                });
+            }
+        }).catch(e => {
+            console.log(e)
+        })
+    }
+}
